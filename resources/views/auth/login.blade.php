82eebf2f5998df1c789c8f09a4380a7bd7cc9f32
Florent <pelletier.ft1@gmail.com>
2021-02-01 16:20:21 +0100

    page login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,159 @@
-@extends('layouts.app')
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="./images/icon.ico">
+  <link rel="icon" type="image/png" href="./images/icon.ico">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    {{ __('Bulletinfini - Authentification') }}
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+  <!-- CSS Files -->
+  <link href="./css/bootstrap.min.css" rel="stylesheet" />
+  <link href="./css/login.css" rel="stylesheet" />
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+</head>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+<body class="login-page sidebar-collapse">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg bg-primary fixed-top navbar-transparent" color-on-scroll="200" style="background-color:transparent!important;">
+    <div class="container" style="height: 70%;">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+      <div class="dropdown button-dropdown">
+        <a href="" class="dropdown-toggle" id="navbarDropdown" data-toggle="dropdown">
+          <span class="button-bar"></span>
+          <span class="button-bar"></span>
+          <span class="button-bar"></span>
+        </a>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+      </div>
+      <div class="navbar-translate">
+        <a class="navbar-brand" rel="tooltip" title="Bienvenue !" data-placement="bottom">
+          {{ __('Bulletinfini') }}
+        </a>
+        <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-bar top-bar"></span>
+          <span class="navbar-toggler-bar middle-bar"></span>
+          <span class="navbar-toggler-bar bottom-bar"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="" style="background: linear-gradient(#38422b 0%, #000 80%);">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Suis nous Twitter" data-placement="bottom" href="" target="_blank">
+              <i></i>
+              <p class="d-lg-none d-xl-none"></p>
+            </a>
+          </li>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+        </ul>
+      </div>
     </div>
-</div>
-@endsection
+  </nav>
+  <!-- End Navbar -->
+
+  <div class="page-header clear-filter" filter-color="yellow">
+    <div class="page-header-image" style="background-image:url(./images/task-list.png)"></div>
+    <!-- <a href="https://iconscout.com/illustrations/developer-team" target="_blank">Developer Team Illustration</a> by <a href="https://iconscout.com/contributors/delesign" target="_blank">Delesign Graphics</a>-->
+    <div class="content">
+      <div class="container">
+        <div class="col-md-4 ml-auto mr-auto">
+          <div class="card card-login card-plain">
+          <div class="card-header text-center">
+              <div class="logo-container">
+                <img src="./images/icon.ico" alt>
+              </div>
+            </div>
+            @if(session()->has('errors'))
+            <div class="alert alert-danger" role="alert">
+              @foreach($errors->all() as $error)
+              {{$error}}
+              <br>
+              @endforeach
+            </div>
+            @endif
+            <div class="card-body">
+              <form method="POST" action="{{ route('login') }}" autocomplete="off">
+                @csrf
+                <!-- Email -->
+                <div class="input-group no-border input-lg">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="material-icons">face</i>
+                    </span>
+                  </div>
+                  <input type="text" placeholder="E-mail" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                  @error('email')
+                  <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                  </span>
+                  @enderror
+                </div>
+                <!-- Password -->
+                <div class="input-group no-border input-lg">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="material-icons">lock_outline</i>
+                    </span>
+                  </div>
+                  <input type="password" placeholder="Mot de passe" class="form-control" class="form-control @error('password') is-invalid @enderror" name="password" required>
+                  @error('password')
+                  <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                  </span>
+                  @enderror
+                </div>
+                <!-- -->
+            </div>
+            <!-- Login -->
+            <div class="card-footer text-center">
+              <button type="submit" class="btn btn-primary btn-round btn-lg btn-block">{{ __('Connexion') }}</button>
+              <div class="pull-right">
+                @if (Route::has('password.request'))
+                <h6> <a class="link">
+                    <!-- href="{{ route('password.request') }}" -->
+                    {{ __('Mot de passe oublié?') }}
+                  </a></h6>
+                @endif
+              </div>
+              <div class="pull-left">
+                <h6> <a class="link">
+                    <!-- href="{{ route('password.request') }}" -->
+                    {{ __('Créer un compte') }}
+                  </a></h6>
+              </div>
+            </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="footer">
+      <div class=" container ">
+        <nav>
+          <ul>
+            <li>
+             
+            </li>
+            <li>
+              <a href="">
+                CONTACT
+              </a>
+            </li>
+
+          </ul>
+        </nav>
+        <div class="copyright" id="copyright">
+          &copy;
+          <script>
+            document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+          </script>
+        </div>
+      </div>
+    </footer>
+  </div>
+</body>
