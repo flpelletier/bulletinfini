4098965dd5278e785c97b4cda3b2cd163f72c6fc
Florent <pelletier.ft1@gmail.com>
2021-02-03 15:58:27 +0100

    Maj logo

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -9,8 +9,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <link rel="apple-touch-icon" sizes="76x76" href="./images/icon.ico">
-    <link rel="icon" type="image/png" href="./images/icon.ico">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{url('./images/icon.ico')}}">
+    <link rel="icon" type="image/png" href="{{url('./images/icon.ico')}}">
     <title>Bulletinfini | Administration</title>
 
     <!-- Font Awesome Icons -->
