e9631e4aceb9bcfdbb62ad91a1fec5c549a779eb
Florent <pelletier.ft1@gmail.com>
2021-03-10 15:26:40 +0100

    MAJ USERS ID

--- resources/views/management/promotion/promotion.blade.php
+++ resources/views/management/promotion/promotion.blade.php
@@ -8,7 +8,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Promotion : {{$promotion->intitule}}</h1>
+                        <h1>Promotion {{$promotion->id}} : {{$promotion->intitule}} </h1>
                     </div>
                 </div>
             </div><!-- /.container-fluid -->
